b72afe5ba21d21872fc48fa84d4661708c4aed97
vtsign <hethongkyketvanban@gmail.com>
2021-09-22 17:51:02 +0700

    first commit

---
+++ src/main/java/tech/vtsign/documentservice/DocumentServiceApplication.java
@@ -0,0 +1,15 @@
+package tech.vtsign.documentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DocumentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentServiceApplication.class, args);
+    }
+
+}
