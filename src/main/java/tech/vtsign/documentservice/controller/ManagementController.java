435bd951e008bae51213e1f6706f6b29f13dc58e
tuan2012 <phampham1109@gmail.com>
2021-12-22 10:07:40 +0700

    add get transactions and count all contract

--- src/main/java/tech/vtsign/documentservice/controller/ManagementController.java
+++ src/main/java/tech/vtsign/documentservice/controller/ManagementController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.vtsign.documentservice.model.CountContractDto;
+import tech.vtsign.documentservice.model.DocumentStatus;
 import tech.vtsign.documentservice.service.ContractService;
 import tech.vtsign.documentservice.utils.DateUtil;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 
 @RestController
@@ -23,10 +27,10 @@ public class ManagementController {
     private final ContractService contractService;
 
     @GetMapping("/count-contract")
-    public ResponseEntity<?> countContract(@RequestParam(value = "type", defaultValue = "week") String type) {
+    public ResponseEntity<?> countContract(@RequestParam(name = "type", defaultValue = "week") String type) {
         CountContractDto countContractDto = new CountContractDto();
 
-        if("all".equals(type)) {
+        if ("all".equals(type)) {
             countContractDto.setSent(contractService.countAllContract());
             countContractDto.setCompleted(contractService.countAllContractCompleted());
         } else {
@@ -39,7 +43,19 @@ public class ManagementController {
     }
 
     @GetMapping("/statistic-contract")
-    public ResponseEntity<?> statisticUser(@RequestParam(value = "type", defaultValue = "week") String type) {
+    public ResponseEntity<?> statisticUser(@RequestParam(name = "type", defaultValue = "week") String type) {
         return ResponseEntity.ok(contractService.getStatistic(type));
+    }
+
+    @GetMapping("/count-all-contract")
+    public ResponseEntity<?> countContracts(@RequestParam(name = "id") UUID userId) {
+
+
+        Map<String, Object> result = new HashMap<>();
+        result.put(DocumentStatus.COMPLETED, contractService.countAllByUserAndStatus(userId, DocumentStatus.COMPLETED));
+        result.put(DocumentStatus.WAITING, contractService.countAllByUserAndStatus(userId, DocumentStatus.WAITING));
+        result.put(DocumentStatus.ACTION_REQUIRE, contractService.countAllByUserAndStatus(userId, DocumentStatus.ACTION_REQUIRE));
+        result.put(DocumentStatus.DELETED, contractService.countAllByUserAndStatus(userId, DocumentStatus.DELETED));
+        return ResponseEntity.ok(result);
     }
 }
